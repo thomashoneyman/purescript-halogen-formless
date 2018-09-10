c6f4cfe67222c900769b3aef570db83ca3595033
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-09-10 16:27:26 -0700

    Rename user form to UserForm

--- example/external-components/Spec.purs
+++ example/external-components/Spec.purs
@@ -10,12 +10,12 @@ import Formless as F
 
 -- We can easily reclaim our ideal User type by picking the
 -- parsed outputs from the FormRow like this:
-type User = Record (FormRow F.OutputType)
+type User = Record (UserFormRow F.OutputType)
 
-newtype Form r f = Form (r (FormRow f))
-derive instance newtypeForm' :: Newtype (Form r f) _
+newtype UserForm r f = UserForm (r (UserFormRow f))
+derive instance newtypeUserForm' :: Newtype (UserForm r f) _
 
-type FormRow f =
+type UserFormRow f =
   ( name     :: f V.FieldError String         String
   , email    :: f V.FieldError (Maybe String) V.Email
   , whiskey  :: f V.FieldError (Maybe String) String
@@ -23,15 +23,15 @@ type FormRow f =
   )
 
 -- | You'll usually want symbol proxies for convenience
-prx :: F.SProxies Form
-prx = F.mkSProxies $ F.FormProxy :: F.FormProxy Form
+prx :: F.SProxies UserForm
+prx = F.mkSProxies $ F.FormProxy :: F.FormProxy UserForm
 
 -- | You can generate your initial inputs
-initialInputs :: Form Record F.InputField
-initialInputs = F.mkInputFields $ F.FormProxy :: F.FormProxy Form
+initialInputs :: UserForm Record F.InputField
+initialInputs = F.mkInputFields $ F.FormProxy :: F.FormProxy UserForm
 
-validators :: ∀ m. MonadEffect m => Form Record (F.Validation Form m)
-validators = Form
+validators :: ∀ m. MonadEffect m => UserForm Record (F.Validation UserForm m)
+validators = UserForm
   { name: V.minLength 7
     -- Unpacks the Maybe value, then checks the email format, then verifies it is not in use
     -- monadically.
--- example/external-components/Types.purs
+++ example/external-components/Types.purs
@@ -5,20 +5,20 @@ import Prelude
 import Data.Maybe (Maybe)
 import Effect.Aff (Aff)
 import Example.App.UI.Typeahead as TA
-import Example.ExternalComponents.Spec (Form)
+import Example.ExternalComponents.Spec (UserForm)
 import Formless as F
 
 ----------
 -- Component
 
 data Query a
-  = Formless (F.Message Query Form) a
+  = Formless (F.Message Query UserForm) a
   | Typeahead Slot (TA.Message Maybe String) a
   | Reset a
 
 type State = Unit
 
-type ChildQuery = F.Query Query (TA.Query String) Slot Form Aff
+type ChildQuery = F.Query Query (TA.Query String) Slot UserForm Aff
 type ChildSlot = Unit
 
 data Slot
