b3f884cbf69a6f130e1f0d0e925766a12a813774
Thomas Honeyman <admin@thomashoneyman.com>
2020-08-10 21:00:27 -0700

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ let
     repo = "easy-purescript-nix";
     rev = "7ff5a12af5750f94d0480059dba0ba6b82c6c452";
     sha256 = "0af25dqhs13ii4mx9jjkx2pww4ddbs741vb5gfc5ckxb084d69fq";
-  }) {};
+  }) { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation {
   name = "halogen-formless";
