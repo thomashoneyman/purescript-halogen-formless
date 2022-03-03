44c8b7ca512d7bb79490879a0e8db3301c12c15c
Thomas Honeyman <hello@thomashoneyman.com>
2022-03-03 13:48:56 -0800

    Update Nix dependencies

--- shell.nix
+++ shell.nix
@@ -6,8 +6,8 @@ let
   pursPkgs = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "13ace3addf14dd9e93af9132e4799b7badfbe99e";
-    sha256 = "1gva113kyygjhn9i92vg6cyj15vhyfhq7haq51cvp4xdz4j0q4xn";
+    rev = "aa72388ca0fb72ed64467f59a121db1f104897db";
+    sha256 = "1j37v3ncnakhq7p4l2vqdn4li8bgwcc8cd2hk2fblxhnlglikgx2";
   }) { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation {
